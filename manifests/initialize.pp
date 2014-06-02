512361e729049bcad27bf14211b6ffaf04ed64c9
Donald Clark Jackson <dcj@clark-communications.com>
2014-06-01 22:51:49 -0700

    tweaks

--- manifests/initialize.pp
+++ manifests/initialize.pp
@@ -1,12 +1,12 @@
 class ezjail::initialize ( 
-  $os_version = '10.0-RELEASE' 
+  $os_version = undef,
   ) {
 
   include ezjail::params
 
   exec { 'ezjail_admin_install':
-    command  => "/usr/local/bin/ezjail-admin install -mp -r ${os_version}",
-    creates => "/usr/jails/basejail",
+    command => "/usr/local/bin/ezjail-admin install -mp -r $::ezjail::params::os_version_default",
+    creates => "$::ezjail::params::jail_root/basejail",
   }
 
   file_line { 'ezjail_enable':
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,3 +1,4 @@
 class ezjail::params {
   $jail_root = '/usr/jails'
+  $os_version_default = '10.0-RELEASE'
   }
