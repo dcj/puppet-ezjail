2e9710296de9c5353c2ed3c5c5daf7a2c30a245d
Donald Clark Jackson <dcj@clark-communications.com>
2014-06-01 21:47:41 -0700

    more fixes

--- manifests/initialize.pp
+++ manifests/initialize.pp
@@ -1,11 +1,11 @@
 class ezjail::initialize ( 
-  $os_version = 'FreeBSD 10.0-RELEASE' 
+  $os_version = '10.0-RELEASE' 
   ) {
 
   include ezjail::params
 
   exec { 'ezjail_admin_install':
-    command  => "/usr/local/bin/ezjail-admin install -mp -r \"${os_version}\" ",
+    command  => "/usr/local/bin/ezjail-admin install -mp -r ${os_version}",
     creates => "/usr/jails/basejail",
   }
 
