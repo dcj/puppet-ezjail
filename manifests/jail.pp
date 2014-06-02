78d831fb0ae682b800ad04ce3dacb164bad0be23
Donald Clark Jackson <dcj@clark-communications.com>
2014-06-01 21:26:45 -0700

    change params to jail

--- manifests/jail.pp
+++ manifests/jail.pp
@@ -1,28 +1,27 @@
 define ezjail::jail ( 
-  $jail_name = undef,
-  $jail_ip   = undef,
+  $ip   = undef,
   ) {
 
   include ezjail
                          
-  exec { "ezjail_${jail_name}":
-    command  => "/usr/local/bin/ezjail-admin create -c zfs ${jail_name} ${jail_ip}",
-    creates  => "$::ezjail::params::jail_root/${jail_name}",
+  exec { "ezjail_${name}":
+    command  => "/usr/local/bin/ezjail-admin create -c zfs ${name} ${ip}",
+    creates  => "$::ezjail::params::jail_root/${name}",
     require  => Class [ 'ezjail::initialize' ],
   }
 
-  service { "ezjail_${jail_name}":
+  service { "ezjail_${name}":
     ensure    => running,
     name      => 'ezjail',
     enable    => true,
     hasstatus => false,
-    start     => "/usr/local/bin/ezjail-admin start ${jail_name}",
-    stop      => "/usr/local/bin/ezjail-admin stop ${jail_name}",
-    restart   => "/usr/local/bin/ezjail-admin restart ${jail_name}",
-    status    => "/usr/local/bin/ezjail-admin console -e /usr/bin/true ${jail_name}",
+    start     => "/usr/local/bin/ezjail-admin start ${name}",
+    stop      => "/usr/local/bin/ezjail-admin stop ${name}",
+    restart   => "/usr/local/bin/ezjail-admin restart ${name}",
+    status    => "/usr/local/bin/ezjail-admin console -e /usr/bin/true ${name}",
   }
 
-  Exec[ "ezjail_${jail_name}" ] -> Service [ "ezjail_${jail_name}" ]
+  Exec[ "ezjail_${name}" ] -> Service [ "ezjail_${name}" ]
 
 }
 
